201e0c86f0c6842abcc62f12ad37238d38bb9bb2
Makentoshe <mkliverout@gmail.com>
2021-02-27 19:09:38 +0300

    feat(stage 1): Implement a reference solution

---
+++ login/stage1/src/test/java/org/hyperskill/login/ExampleUnitTest.kt
@@ -0,0 +1,57 @@
+package org.hyperskill.login
+
+import android.widget.Button
+import android.widget.TextView
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class ExampleUnitTest {
+
+    private val activityController = Robolectric.buildActivity(MainActivity::class.java)
+
+    @Test
+    fun testShouldCheckStartButtonExist() {
+        val activity = activityController.setup().get()
+
+        val message = "does view with id \"startButton\" placed in activity?"
+        assertNotNull(message, activity.findViewById<Button>(R.id.startButton))
+    }
+
+    @Test
+    fun testShouldCheckResetButtonExist() {
+        val activity = activityController.setup().get()
+
+        val message = "does view with id \"resetButton\" placed in activity?"
+        assertNotNull(message, activity.findViewById<Button>(R.id.resetButton))
+    }
+
+    @Test
+    fun testShouldCheckTextViewExist() {
+        val activity = activityController.setup().get()
+
+        val message = "does view with id \"textView\" placed in activity?"
+        assertNotNull(message, activity.findViewById<TextView>(R.id.textView))
+    }
+
+    @Test
+    fun testShouldCheckStartButtonText() {
+        val activity = activityController.setup().get()
+
+        val message = "in button property \"text\""
+        assertEquals(message, "Start", activity.findViewById<Button>(R.id.startButton).text)
+    }
+
+    @Test
+    fun testShouldCheckResetButtonText() {
+        val activity = activityController.setup().get()
+
+        val message = "in button property \"text\""
+        assertEquals(message, "Reset", activity.findViewById<Button>(R.id.resetButton).text)
+    }
+
+}
