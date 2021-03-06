9372c3b2780dc9a4b8cc9f8fe6e581bbc6f34779
Makentoshe <mkliverout@gmail.com>
2021-03-06 14:58:01 +0300

    feat(stage 2): Add simple stage description and the reference solution

--- login/stage2/src/main/java/org/hyperskill/login/MainActivity.kt
+++ login/stage2/src/main/java/org/hyperskill/login/MainActivity.kt
@@ -4,8 +4,18 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
+
+    private val cookieJar = CustomCookieJar()
+    private val client = OkHttpClient.Builder().cookieJar(cookieJar).build()
+    private val thread = thread(start = false, isDaemon = true) {
+        val request = Request.Builder().url("https://hyperskill.org").build()
+        val response = client.newCall(request).execute()
+        println(response)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +36,22 @@ class MainActivity : AppCompatActivity() {
             message_error.visibility = View.GONE
             login_progress.visibility = View.VISIBLE
             login_button.isEnabled = false
+
+            thread.start()
         }
     }
+
+    private class CustomCookieJar : CookieJar {
+
+        private val cookies = HashMap<HttpUrl, ArrayList<Cookie>>()
+
+        override fun loadForRequest(url: HttpUrl): List<Cookie> {
+            return cookies.values.flatten()
+        }
+
+        override fun saveFromResponse(url: HttpUrl, cookies: List<Cookie>) {
+            this.cookies[url] = (this.cookies[url] ?: ArrayList()).apply { addAll(cookies) }
+        }
+    }
+
 }
